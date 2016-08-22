50655b435b323d663e048a681947bd7fa43be220
chimerarocks <joaopedrodslv@gmail.com>
2016-08-22 18:18:16 -0300

    first commit

---
+++ src/ChimeraRocks/Post/Providers/PostServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ChimeraRocks\Post\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PostServiceProvider extends ServiceProvider
+{
+	public function boot()
+	{
+		$this->publishes([
+			__DIR__ . '/../../../resources/migrations/' => base_path('database/migrations')
+			],'migrations');
+
+		$this->loadViewsFrom(__DIR__ . '/../../../resources/views/chimerapost', 'chimerapost');
+
+		require __DIR__ . '/../Routes.php';
+	}
+
+	/**
+     * Register the service provider.
+     *
+     * @return void
+     */
+	public function register()
+	{
+		
+	}
+}
---
+++ src/resources/migrations/2016_08_22_1809_create_chimerarocks_posts_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChimerarocksPostsTable
+{
+	public function up()
+	{
+		Schema::create('chimerarocks_posts', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('title');
+			$table->text('content');
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('chimerarocks_posts');
+	}
+}
